83955f9bc6d93bfdc7e233d763444da98e1e6a8e
Aaron <114060853+hecabre@users.noreply.github.com>
2024-06-10 01:17:49 -0600

    finished 2

--- server/src/sockets/socketHandlers.js
+++ server/src/sockets/socketHandlers.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import { authRequired } from "../api/middlewares/validateToken.js";
 
 let users = 0;
-let bombState = false;
+let bombState1 = false;
+let bombState2 = false;
 
 export const handleSocketConnections = (server) => {
   dotenv.config();
@@ -11,18 +12,18 @@ export const handleSocketConnections = (server) => {
     cors: {
       origin: "http://localhost:5173",
       credentials: true,
-      methods: ["GET", "POST"], // Métodos permitidos
-      allowedHeaders: ["Authorization"], // Encabezados permitidos
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Authorization"],
     },
   });
 
-  
   io.on("connection", (socket) => {
     users++;
     console.log("User connected. Total users: " + users);
     io.emit("usersCount", users);
 
-    socket.emit("initialBombState", bombState);
+    // Emitimos el estado inicial de ambas bombas
+    socket.emit("initialBombState", { bomb1: bombState1, bomb2: bombState2 });
 
     socket.on("disconnect", () => {
       users--;
@@ -30,10 +31,24 @@ export const handleSocketConnections = (server) => {
       io.emit("usersCount", users);
     });
 
-    socket.on("turnOnBomb", (newBombState) => {
-      bombState = newBombState;
-      console.log("Bomb state: ", bombState);
-      io.emit("bomb", bombState);
+    socket.on("turnOnBomb1", () => {
+      if (!bombState2) {
+        bombState1 = !bombState1;
+        console.log("Bomb 1 state: ", bombState1);
+        io.emit("bomb1", bombState1);
+      } else {
+        console.log("Bomb 2 is already on, cannot turn on Bomb 1");
+      }
+    });
+
+    socket.on("turnOnBomb2", () => {
+      if (!bombState1) {
+        bombState2 = !bombState2;
+        console.log("Bomb 2 state: ", bombState2);
+        io.emit("bomb2", bombState2);
+      } else {
+        console.log("Bomb 1 is already on, cannot turn on Bomb 2");
+      }
     });
   });
 };
